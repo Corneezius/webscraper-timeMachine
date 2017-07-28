fe705733085b4b81294d3ebf4fca4273152b6e11
Corneezius <cfr2116@columbia.edu>
2017-07-28 04:59:58 -0400

    start writing function to ouput json in separate file

--- server.js
+++ server.js
@@ -66,6 +66,11 @@ url = 'http://web.archive.org/web/timemap/json/http://www.coinbase.com/about';
     //
     // },90000);
 
+    // fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+    // console.log('File successfully written! - Check your project directory for the output.json file');
+
+})
+
 });
 
 app.listen('8081')
